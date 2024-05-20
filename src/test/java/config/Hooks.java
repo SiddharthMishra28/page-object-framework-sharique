aa59a36fc3536bc41ec4557c6c4693eea4724d60
SiddharthMishra28 <connectwithsiddharthm@gmail.com>
2024-05-20 22:07:58 +0530

    Added reusable utils

--- src/test/java/config/Hooks.java
+++ src/test/java/config/Hooks.java
@@ -9,8 +9,10 @@ import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
 
@@ -26,12 +28,22 @@ public class Hooks {
 	
 	@BeforeTest
 	public void beforeTest() {
+		
+	}
+	
+	@BeforeMethod
+	public void beforeMethod() {
 		this.launchBrowser();
+	}
+	
+	@AfterMethod
+	public void afterMethod() {
+		this.closeBrowser();
 	}
 	
 	@AfterTest
 	public void afterTest() {
-		this.closeBrowser();
+		
 	}
 	
 	@AfterSuite
@@ -40,6 +52,9 @@ public class Hooks {
 		// EXIT WEBDRIVER INSTANCE
 		// PUBLISH REPORT
 		// POST COMPLETION STEPS
+		if(this.driver != null) {
+			this.driver.quit();
+		}
 	}
 	
 	/**
--- src/test/java/utils/UIAutomationUtils.java
+++ src/test/java/utils/UIAutomationUtils.java
@@ -1,6 +1,30 @@
 package utils;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class UIAutomationUtils {
 	// IMPLEMENTATION FOR RE-USABLE METHODS
 	
+	public WebDriver driver;
+	
+	public UIAutomationUtils(WebDriver driver) {
+		this.driver = driver;
+	}
+	
+	/**
+	 * 
+	 * @param url
+	 */
+	public void navigateToUrl(String url) {
+		this.driver.get(url);
+	}
+	
+	public void clickElement(WebElement element) {
+		element.click();
+	}
+	
+	public void type(WebElement element, String text) {
+		element.sendKeys(text);
+	}
 }
